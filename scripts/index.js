7d9faf5e64971a1b61ada4409f752930f5fbfa15
driuki <dargav999@gmail.com>
2019-10-24 13:23:09 +0300

    Pataisymai

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+$('#kurimas').click(function(){
+    window.location = '../veikla/svetainiu-kurimas.html';
+});
+
+$('#seo').click(function(){
+    window.location = '../veikla/seo.html';
+});
+
+$('#prieziura').click(function(){
+    window.location = '../veikla/prieziura.html';
+});
